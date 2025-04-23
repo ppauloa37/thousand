9a19075de7c7a0d17fc22f574e497e00531a28c2
Paulfox <ppauloa37@gmail.com>
2025-04-23 16:56:07 -0300

    Update thousand.js

--- package/canva/thousand.js
+++ package/canva/thousand.js
@@ -1,88 +1,51 @@
+//draw functions
+console.log("Esta a usar Thousand.js em portugues")
+
+/**
+ * @type {{
+*   canvas: HTMLCanvasElement | null,
+*   painttype: any,
+*   canvasdef: (canvastype: string) => void
+* }}
+*/
+
 var th = {
-canvas: null, 
-p: null,
-init(Idcanvaid) {
-canvas = document.getElementById(Idcanvaid);
- if (!canvas) {
-console.error("Object element cannot find canvas");
-return;
- }
+    canvas: null,
+    p: null,
+    hour: null,
 
-},
- ctx(typecan) {
-    if (typecan.toLowerCase() === "3d" || typecan === "3D") {typecan === "webgl"}
-    this.p = canvas.getContext(typecan);
-},
- boxcolid(x,y,x1,y1) {
-
-},
-
-
-rect(x, y, w, h, c) {
-    if (this.p) {
-        this.p.fillStyle = c;
-        this.p.fillRect(x, y, w, h);
-    } else {
-        console.error("ctx its not define", {
-ctx: ctx,
-rect: rect,
-         
-        });
-    }
-},
-    text(txt, x, y, c, tmh) {
-        if (this.p) {
-            this.p.fillStyle = c;
-            this.p.font = tmh + "px sans-serif";
-            this.p.fillText(txt, x, y);
-        } else if (this.x === 0 || this.y === 0 || this.tmh === 0) {
-            console.error("this text not will never be found");
+    canvasdef: function(canvastype) {
+        if (canvastype) {
+            this.canvas = document.getElementById(canvastype)
+                if (!this.canvas) {
+                    console.error("Objeto não encontrado");
+                }
+        } else {
+            console.error("ID não fornecido");
         }
-        
     },
-    cube(x,y,z,c1,c2,c3) {
-        t = z
-  this.p.fillStyle = c1;
-  this.p.beginPath();
-  this.p.moveTo(x, y);
-  this.p.lineTo(x + t, y - t / 2);
-  this.p.lineTo(x + 2 * t, y);
-  this.p.lineTo(x + t, y + t / 2);
-  this.p.closePath();
-  this.p.fill();
-
-  this.p.fillStyle = c2;
-  this.p.beginPath();
-  this.p.moveTo(x, y);
-  this.p.lineTo(x + t, y + t / 2);
-  this.p.lineTo(x + t, y + t + t / 2);
-  this.p.lineTo(x, y + t);
-  this.p.closePath();
-  this.p.fill();
-
-  
-  this.p.fillStyle = c3;
-  this.p.beginPath();
-  this.p.moveTo(x + 2 * t, y);
-  this.p.lineTo(x + t, y + t / 2);
-  this.p.lineTo(x + t, y + t + t / 2);
-  this.p.lineTo(x + 2 * t, y + t);
-  this.p.closePath();
-  this.p.fill();
+    ctx: function(ctxtype) {
+        if (this.canvas) {
+            this.p = this.canvas.getContext(ctxtype);
+        } 
+        if (!this.p) {
+            console.error("Contexto não encontrado");
+        } 
     },
-    ellipse(x,y,w,h) {
-    if (this.p) {
-        this.p.beginPath();
-        this.p.ellipse(x, y, w / 2, h / 2, 0, 0, Math.PI * 2);
-        this.p.fill();
-    } else {
-        console.error("ctx cannot define");
+    rect: function(x,y,w,h,color) {
+        this.p.fillStyle = color;
+        this.p.fillRect(x,y,w,h);
+    },
+    getHour: function() {
+        hour = new Date.getHour();
+    },
+    useThousandtype: function(vari,number) {
+        vari = number + "thousandtext";
+    },
+    text: function(textcontent,x,y,font,px,color) {
+        this.p.fillStyle = color;
+        this.p.font = px + " " + font;
+        this.p.fillText(textcontent,x,y);
+        console.log("esta usando texto");
     }
-    }
-}
-// def function the bibliotec public
-
-window.th = th;
-
-
-
+};
